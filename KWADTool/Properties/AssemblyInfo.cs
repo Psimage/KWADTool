19e3eaadd697b690b344badf8f5cfe610d25c194
Yarosbug <scourgegames@gmail.com>
2015-09-05 16:16:59 +0300

    changed version number to 1.0.1

--- KWADTool/Properties/AssemblyInfo.cs
+++ KWADTool/Properties/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using CommandLine;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
 
 // from CommandLineParser.Text
 [assembly: AssemblyLicense(
